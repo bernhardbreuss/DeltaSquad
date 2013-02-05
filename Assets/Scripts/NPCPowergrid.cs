119df45dd0dbe90834f5a290a1f9a192ae17d299
Bernhard Breuß <bernhard.breuss@students.fhv.at>
2013-02-05 11:22:43 +0100

    increasing/decreasing money on buying/selling energy

--- Assets/Scripts/NPCPowergrid.cs
+++ Assets/Scripts/NPCPowergrid.cs
@@ -1,9 +1,13 @@
-using System;
+using UnityEngine;
 
 public class NPCPowergrid : Powergrid
 {
 	public float SellingFrequency = 50.5f;
 	public float BuyingFrequency = 49.5f;
+	
+	private float MoneyFactor = 1.0f;
+	private float TradeFee = 0.1f;
+	private float MaxHz = 52.0f;
 	
 	public PlayerPowergrid PlayerPowergrid;
 	
@@ -21,19 +25,75 @@ public class NPCPowergrid : Powergrid
 	{
 		float difference = (CurrentChange() * factor);
 		
-		if (Frequency >= SellingFrequency && difference > 0.0f) {
+		if (difference > 0.0f) {
+			// Player is sellying energy to the NPC
+			if (OwnFrequency >= SellingFrequency) {
+				if (ForeignEnergy < 0.0f) {
+					if ((ForeignEnergy + difference) > 0.0f) {
+						difference = -ForeignEnergy;
+					}
+				} else {
+					return;
+				}
+			} else if (Frequency >= SellingFrequency) {
+				return;
+			}
+		} else if (difference < 0.0f) {
+			// Player buying energy from NPC
+			if (OwnFrequency < BuyingFrequency) {
+				if (ForeignEnergy > 0.0f) {
+					if ((ForeignEnergy + difference) < 0.0f) {
+						difference = -ForeignEnergy;
+					}
+				} else {
+					return;
+				}
+			} else if (Frequency < BuyingFrequency) {
+				return;
+			}
+		} else {
 			return;
 		}
-		if (Frequency < BuyingFrequency && difference < 0.0f) {
-			return;
-		}
-		
+						
 		if ((PlayerPowergrid.ProducedEnergy + PlayerPowergrid.ForeignEnergy - difference) < 0.0f) {
+			// to less energy
 			return;
 		}
 		
 		ForeignEnergy += difference;
-		PlayerPowergrid.ForeignEnergy -= difference;	
+		PlayerPowergrid.ForeignEnergy -= difference;
+	}
+	
+	protected override void Start ()
+	{
+		base.Start ();
+	}
+	
+	protected override void Update ()
+	{
+		base.Update ();
+		
+		if (ForeignEnergy == 0.0f) {
+			return;
+		}
+		
+		float money = 0.0f;
+		
+		if (ForeignEnergy  < 0.0f) {
+			money = ((MaxHz - OwnFrequency) * -MoneyFactor);
+		} else if (ForeignEnergy > 0.0f) {
+			money = ((50.0f - OwnFrequency) * MoneyFactor);
+		}
+		
+		money -= TradeFee;		
+		money *= Time.deltaTime;
+		
+		if ((PlayerPowergrid.Euro + money) < 0.0f) {
+			PlayerPowergrid.ForeignEnergy += ForeignEnergy;
+			ForeignEnergy = 0.0f;
+		} else {	
+			PlayerPowergrid.changeAmountEuro(money);
+		}
 	}
 }
 
