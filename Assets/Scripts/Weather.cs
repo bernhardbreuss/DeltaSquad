797752d644f2a60aaa102c6e0d671fa23f2eec15
Bernhard Breuß <bernhard.breuss@students.fhv.at>
2013-02-06 19:15:30 +0100

    finished weather in WaveManager

--- Assets/Scripts/Weather.cs
+++ Assets/Scripts/Weather.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Weather : MonoBehaviour {
 	
 	private float _energyProductionRate;
+	private float _changeRemain;
+	private float _change;
+	private float _targetValue;
 	
 	public float EnergyProductionRate { 
 		get {
@@ -27,10 +30,22 @@ public class Weather : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (_changeRemain > 0.0f) {
+			EnergyProductionRate -= (_change * Time.deltaTime);
+			_changeRemain -= Time.deltaTime;
+		} else if (_changeRemain < 0.0f) {
+			_changeRemain = 0.0f;
+			EnergyProductionRate = _targetValue;
+		}
 	}
 	
 	public void Reset(){
 		EnergyProductionRate = 1f;
+	}
+	
+	public void ChangeTo(float newValue, float time) {
+		_change = (EnergyProductionRate - newValue) / time;
+		_changeRemain = time;
+		_targetValue = newValue;
 	}
 }
