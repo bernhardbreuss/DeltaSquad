f2d2563429a5a87f34a9aa5e0870dfda487465db
Paddy Doyle <paddyd19@gmail.com>
2013-02-07 11:36:34 +0000

    Cars wait to leave charging stations if the way is not clear

--- Assets/Scripts/ChargingStation.cs
+++ Assets/Scripts/ChargingStation.cs
@@ -42,7 +42,11 @@ public class ChargingStation: MonoBehaviour {
 	
 	public IEnumerator ChargingFinished() {
 		yield return new WaitForSeconds(chargeTime);
-		powergrid.ConsumedEnergy -= rechargingRate;	
+		powergrid.ConsumedEnergy -= rechargingRate;
+		if ( !_car.freeToMove() )
+		{
+			yield return new WaitForSeconds(1);
+		}
 		_car.transform.Translate(-35f,0f,0f);
 		_car.ChangeState(new StateLeaving(_car));
 		_car = null;
